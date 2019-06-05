64955128d996b45acf199b14c3ef4d16c6b114e2
Miguel Maestre <maestre.95@gmail.com>
2019-06-04 22:55:17 -0400

    estadisticas

--- main.js
+++ main.js
@@ -15,12 +15,19 @@ var stopConditionValue = 4000
 var arrivalCount = 0
 var departureCount = 0
 var maxQueue = 0
+var maxQueueTime = 0
 var lastArrived = 0
 var TBA_Acum = 0;
 var lastDeparture = 0
 var TBD_Acum = 0 
 var idleTime = 0
 var generatedEvent
+var clients = []
+
+var inSistemTimeAverage = 0
+var ISTACount = 0
+var QueueAverage = 0
+var QACount = 0
 
 const weibull = (g, a, b, u) => {
   return (g + b * (-Math.log(u)) ** (1 / a))
@@ -35,6 +42,7 @@ const execE2 = () => {
   let res = { activeEvent: null, generated: '', ris: [] }
   res.activeEvent = eventList.splice(0, 1)[0]
   clock = res.activeEvent.time
+  if (clients.length) clients[departureCount-1].outTime = clock
   let newE2 = null
   let newE1 = null
   if (entities > 0) {
@@ -67,8 +75,11 @@ const execE2 = () => {
 const execE1 = () => {
   entities += 1
   arrivalCount += 1
-  let res = { activeEvent: null, generated: null, ris: [] }
+  let res = { activeEvent: null, generated: [], ris: [] }
   res.activeEvent = eventList.splice(0, 1)[0]
+  clients.push({inTime: res.activeEvent.time, outTime: 0})
+  TBA_Acum += res.activeEvent.time - lastArrived
+  lastArrived = res.activeEvent.time
   clock = res.activeEvent.time
   res.ris.push(Math.random())
   let newE1 = generateEvent(1, res.ris[0])
@@ -80,8 +91,10 @@ const execE1 = () => {
     eventList.push(newE2)
   }
 
-  res.generated = `E1<sub>(${newE1.time.toFixed(2)})</sub>${newE2 ? '/E2<sub>(' + newE2.time.toFixed(2) + ')</sub>' : ''}`
-
+  res.generated.push(`E1<sub>(${newE1.time.toFixed(2)})</sub>`)
+  if (newE2){
+    res.generated.push(`E2<sub>(${newE2.time.toFixed(2)})</sub>`)
+  }
   sortEventList()
   return res
 }
@@ -141,13 +154,13 @@ const generateEvent = (t, u) => {
 }
 const printEventRow = (a, b, c, d, e, f) => {
   $('tbody').append(`<tr>
-          <td>${(a).toFixed(2)}</td>
-          <td>${b}</td>
-          <td>${c}</td>
-          <td>${d}</td>
-          <td>${e}</td>
-          <td class="text-left">${f}</td>
-        </tr>`)
+      <td>${a == '-' ? a : (a).toFixed(2)}</td>
+      <td>${b}</td>
+      <td>${c}</td>
+      <td>${d}</td>
+      <td>${e}</td>
+      <td class="text-left">${f}</td>
+    </tr>`)
 }
 const main = () => {
   $('#entradadatos').toggleClass('d-none')
@@ -172,9 +185,16 @@ const main = () => {
 
   sortEventList()
 
+  for(let i =0; i < entities; i++) {
+    clients.push({inTime: 0, outTime: 0})
+  }
+
   printEventRow(clock, '-', entities, '-', '-', printEventList())
   while (!stop) {
-    if (entities && entities > maxQueue) maxQueue = entities - 1
+    if (entities && entities > maxQueue) {
+      maxQueue = entities - 1
+      maxQueueTime = clock
+    }
     if (stopCondition == '1' && stopConditionValue <= arrivalCount) break
     else if (stopCondition == '2' && stopConditionValue <= departureCount) break
     else if (stopCondition == '3' && stopConditionValue <= clock) {
@@ -191,7 +211,14 @@ const main = () => {
         let { generated, ris } = execE1()
         idleTime += clock - prevClock
         sortEventList()
-        printEventRow(clock, 'E1', entities, `${ris == '-' ? ris : ris.length == 1 ? ris[0].toFixed(3) : ris[0].toFixed(3) + '/' + ris[1].toFixed(3)}`, generated, printEventList())
+        if (ris.length > 1) {
+          printEventRow(clock, 'E1', entities, `${ris[0].toFixed(3)}`, generated[0], printEventList().replace(generated[1], ''))
+          printEventRow('-', '-', entities, `${ris[1].toFixed(3)}`, generated[1], printEventList())
+        } else {
+          printEventRow(clock, 'E1', entities, `${ris.length == 1 ? ris[0].toFixed(3) : ris[0].toFixed(3) + '/' + ris[1].toFixed(3)}`, 
+            generated, 
+            printEventList())
+        }
       } else {
         let newEvent = generateEvent(1, Ri)
         eventList.push(newEvent)
@@ -204,7 +231,14 @@ const main = () => {
         let { ris, generated } = execE1()
         sortEventList()
         printEventList()
-        printEventRow(clock, 'E1', entities, `${ris.length == 1 ? ris[0].toFixed(3) : ris[0].toFixed(3) + '/' + ris[1].toFixed(3)}`, generated, printEventList())
+        if (ris.length > 1) {
+          printEventRow(clock, 'E1', entities, `${ris[0].toFixed(3)}`, generated[0], printEventList())
+          printEventRow('-', '-', entities, `${ris[1].toFixed(3)}`, generated[1], printEventList())
+        } else {
+          printEventRow(clock, 'E1', entities, `${ris.length == 1 ? ris[0].toFixed(3) : ris[0].toFixed(3) + '/' + ris[1].toFixed(3)}`, 
+            generated, 
+            printEventList())
+        }
       } else if (eventList.length && eventList[0].type == 'E2') {
         let { ris, generated } = execE2()
         sortEventList()
@@ -243,15 +277,49 @@ const main = () => {
     $('#patSalida').append(`<tr><td>${i}</td><td>${d[0].toFixed(3)}</td><td>${d[1].toFixed(3)}</td></tr>`)
   })
 
+  let serviceTimeAverage = 0
+  departures.forEach((dep) => {
+    serviceTimeAverage += parseFloat(departures[0])
+  })
+
+  clients.forEach(cli => {
+    if (cli.inTime == 0) return
+    if(cli.outTime>0){
+      ISTACount++;
+      inSistemTimeAverage += cli.outTime - cli.inTime
+    }
+  })
+
+  clients.forEach((cli, i) => {
+    if (i>0) {
+      if (cli.inTime) {
+        QACount++
+        if (cli.inTime>clients[i-1].outTime) return
+        else {
+          QueueAverage += clients[i-1].outTime - cli.inTime
+        }
+      }
+    }
+  })
+
   $('#tabla').append(`
     <div class="col-10 mx-auto mb-1 card px-3 py-2">
       <h4 class="text-center">Resultados</h4> 
       <span><strong>Solicitudes de Servicio:</strong> ${arrivalCount}</span>
-      <span><strong>Entidades Atendidas:</strong> ${departureCount}</span>
-      <span><strong>Maximo de clientes en Cola:</strong> ${maxQueue}</span>
-      <span><strong>Maximo de Entidades en el Sistema:</strong> ${maxQueue+1}</span>
-      <span><strong>Tiempo de Ocio:</strong> ${idleTime.toFixed(2)} Seg</span><span><strong>Tiempo de Uso:</strong> ${(clock - idleTime).toFixed(2)} Seg</span>
-      <span><strong>Eventos pendientes:</strong> ${printEventList()}</span>
+      <span><strong>Clientes atendidos:</strong> ${departureCount}</span>
+      <span><strong>Maximo de Clientes en Cola:</strong> ${maxQueue} a los ${maxQueueTime.toFixed(2)} Seg.</span>
+      <span><strong>Maximo de Clientes en el Sistema:</strong> ${maxQueue+1} a los ${maxQueueTime.toFixed(2)} Seg.</span>
+      <span><strong>Tiempo Promedio entre llegadas:</strong> ${(TBA_Acum/arrivals.length).toFixed(2)} Seg.</span>
+      <span><strong>Tiempo Promedio de Servicio:</strong> ${(serviceTimeAverage/departures.length).toFixed(2)} Seg.</span>
+      <span><strong>Tiempo Promedio de espera en Sistema:</strong> ${(inSistemTimeAverage/ISTACount).toFixed(2)} Seg.</span>
+      <span><strong>Tiempo Promedio de espera en Cola:</strong> ${(QueueAverage/QACount).toFixed(2)} Seg.</span>
+      <span><strong>Tiempo de Uso del Servidor:</strong> ${(clock - idleTime).toFixed(2)} Seg.</span>
+      <span><strong>Tiempo de Ocio del Servidor:</strong> ${(idleTime).toFixed(2)} Seg.</span>
+      <h4 class="text-center mt-2">Estado Final del Sistema</h4> 
+      <p>
+        La simulacion se detuvo a los ${clock.toFixed(2)} segundos, quedando ${(entities>1 || entities == 0) ? entities + ' entidades' : 1 + ' entidad'} en cola. <br>
+        <span><strong>Eventos pendientes:</strong> ${printEventList()}</span>
+      </p>
     </div>
   `)
 }
